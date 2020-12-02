5ce533d657c78a98c1d68cd0edbaa29efc40f0a3
Tendai Nyandoro <tnyandoro@gmail.com>
2020-12-02 11:09:16 +0200

    Add The user controller
    Create new resources
    
     Create new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create]
 end
