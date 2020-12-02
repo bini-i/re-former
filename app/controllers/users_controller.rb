159d5d37aeced1b9f45c30b95f1e7b4c2fe9e169
Tendai Nyandoro <tnyandoro@gmail.com>
2020-12-02 16:26:06 +0200

    Add parmas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,22 @@ class UsersController < ApplicationController
   end
   
   def create 
-    @user = User.new(username: params[:user][:username], email: params[:user][:email],password: params[:user][:password])
+    @user = User.new(user_params)
     
     if @user.save
-      redirect new_user_path
+      redirect_to new_user_path
     else
       render :new
     end
   end 
+
+  private
+  
+  def user_params
+
+  params.require(:user).permit(:username, :email, :password)
+  end
+
 
 end
 
