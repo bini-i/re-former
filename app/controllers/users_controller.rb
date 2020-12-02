ab21030f9d7997a1f63268484a97dab2ae63bf0f
bini-i <binyam.sh1@gmail.com>
2020-12-02 16:55:28 +0300

    Add create action in users_controller.rb and new.html.erb view
    - create action creates and saves a user object to the database
    - view displays a form to create a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
   end
   
   def create 
-  end
+    @user = User.new(username: params[:user][:username], email: params[:user][:email],password: params[:user][:password])
+    
+    if @user.save
+      redirect new_user_path
+    else
+      render :new
+    end
+  end 
 
 end
 
