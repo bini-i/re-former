262c65736ecbfa2c8953c869e3e535c8970729a0
bini-i <binyam.sh1@gmail.com>
2020-12-03 18:59:54 +0300

    Update update action in users_controller.rb and removed index and show routes from routes.rb
    - update action made to redirect only if save is successful

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: %i[index show new create edit update destroy]
+  resources :users, only: %i[new create edit update]
 end
