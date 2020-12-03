5e38a073558e603acb7a06d8eed41159e602bc81
bini-i <binyam.sh1@gmail.com>
2020-12-03 13:10:37 +0300

    Add update and edit actions(methods) in users_controller.rb
    - edit action renders edit view with a form_for form to update the user
    - update action updates user object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
   def new
-
     @user = User.new
-
   end
   
   def create 
@@ -11,11 +13,26 @@ class UsersController < ApplicationController
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user.id)
     else
       render :new
     end
   end 
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+    # @user.update_attributes(user_params)  
+    # @user.save
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 
   private
   
